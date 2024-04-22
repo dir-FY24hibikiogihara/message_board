bd4c16c7ec71cb2aca85d22e5e8b9deb3895ca3d
Hibikiengawa <hibiki.ogihara.x996@dirbato.co.jp>
2024-04-22 09:52:36 +0900

    ページネーションの追加

--- messeage_board/src/controllers/IndexServlet.java
+++ messeage_board/src/controllers/IndexServlet.java
@@ -31,12 +31,26 @@ public class IndexServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	        throws ServletException, IOException {
 	    EntityManager em = DBUtil.createEntityManager();
+	    int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
 
-	    List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+        // 最大件数と開始位置を指定してメッセージを取得
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class)
+                .setFirstResult(15 * (page - 1))
+                .setMaxResults(15)
+                .getResultList();
 
-	    em.close();
+        // 全件数を取得
+            long messages_count = (long)em.createNamedQuery("getMessagesCount", Long.class)
+                   .getSingleResult();
+
+            em.close();
 
 	    request.setAttribute("messages", messages);
+	    request.setAttribute("messages_count", messages_count);     
+        request.setAttribute("page", page);
 	    // フラッシュメッセージがセッションスコープにセットされていたら
         // リクエストスコープに保存する（セッションスコープからは削除）
         if(request.getSession().getAttribute("flush") != null) {
